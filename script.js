380f822288233b70d0c739f5af1c384559864b88
SucioLoco <irelandrj@yahoo.com>
2021-10-18 15:16:48 -0400

    Fetching json data to display

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ const newMovie = document.getElementById("new-movie")
 console.log(newMovie)
 const container = document.getElementById("movie-container")
 console.log(container)
-const url = http://localhost:3000/movies
+const url = "http://localhost:3000/movies"
 
 
-fetch('url'
+fetch(url) 
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        for (let item of data) {
+        let movieCard = document.createElement("div")
+        let movieTitle = document.createElement("div")
+        movieTitle.innerText = item.title
+        }
+    })
