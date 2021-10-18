63a3cd219e14dd7ecb69b88806ab739610f1db50
James Andrews <jbandrew14@gmail.com>
2021-10-18 14:56:02 -0400

    created url variable and started fetch

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const form = document.getElementById("movie-form")
 console.log(form)
-const newMovie = docuemnt.getElementById("new-movie")
+const newMovie = document.getElementById("new-movie")
 console.log(newMovie)
 const container = document.getElementById("movie-container")
 console.log(container)
+const url = http://localhost:3000/movies
+
+
+fetch('url'
