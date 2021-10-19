785cb5b607e75891262f36ab3a551e25bb8d13bc
James Andrews <jbandrew14@gmail.com>
2021-10-19 11:14:50 -0400

    rendered watched boolean with message, and organized code into functions

--- script.js
+++ script.js
@@ -8,26 +8,43 @@ const url = "http://localhost:3000/movies";
 
 // fetching json from database
 fetch(url)
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-    // iterating through data array full of objects
-    for (let item of data) {
-        // creating div containers for moviecard and movie title
-      let movieCard = document.createElement("div");
-      let movieTitle = document.createElement("div");
-      let movieCreated = document.createElement("div");
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+        // iterating through data array full of objects
+        for (let item of data) {
+            // creating div container for moviecard
+            let movieCard = document.createElement("div");
 
-      // setting the innerText of movie title and adding to moviecard
-      movieTitle.innerText = item.title;
-      movieCreated.innerText = item.created_at
-      console.log(movieTitle.innerText);
-      movieCard.appendChild(movieTitle);
-      movieCard.appendChild(movieCreated);
-      
-     
-      container.appendChild(movieCard)
+            // call render functions and add movieCard to container
+            renderTitle(item, movieCard);
+            renderCreated(item, movieCard);
+            renderWatched(item, movieCard);
+            container.appendChild(movieCard);
+        }
+    });
 
+// grab title text from json item and render it in an element
+function renderTitle(movieObj, card) {
+    let movieTitle = document.createElement("div");
+    movieTitle.innerText = movieObj.title;
+    card.appendChild(movieTitle);
+}
 
+// grab created date from json item and render it in an element
+function renderCreated(item, movieCard) {
+    let movieCreated = document.createElement("div");
+    movieCreated.innerText = item.created_at
+    movieCard.appendChild(movieCreated);
+}
+
+// grab watched boolean from json item and render it in an element
+function renderWatched(item, movieCard) {
+    let movieWatched = document.createElement("div");
+    if (item.watched) {
+        movieWatched.innerText = "Watched"
+    } else {
+        movieWatched.innerText = "Not Watched"
     }
-  });
+    movieCard.appendChild(movieWatched);
+}
